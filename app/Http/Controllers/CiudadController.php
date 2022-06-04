d85c87f03c42520476bfd3df889c172820b652cc
Rodrigo <rodrigo0001g@gmail.com>
2022-06-03 22:32:28 -0400

    Arreglo del borrado de acentos

--- app/Http/Controllers/CiudadController.php
+++ app/Http/Controllers/CiudadController.php
@@ -53,7 +53,7 @@ class CiudadController extends Controller
     }
 
     public function textFormat($text){
-        return strtoupper(iconv('utf-8','ASCII//IGNORE//TRANSLIT',$text));
+        return strtoupper($this->replaceTildes($text));
     }
 
     public function setSettlementsStructure($settlements){
@@ -73,5 +73,42 @@ class CiudadController extends Controller
             array_push($result, $element);
         }
         return $result;
+    }
+
+    function replaceTildes($string){
+
+        $string = str_replace(
+            array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+            array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+            $string
+        );
+    
+        $string = str_replace(
+            array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+            array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+            $string );
+    
+        $string = str_replace(
+            array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+            array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+            $string );
+    
+        $string = str_replace(
+            array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+            array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+            $string );
+    
+        $string = str_replace(
+            array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+            array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+            $string );
+    
+        $string = str_replace(
+            array('ñ', 'Ñ', 'ç', 'Ç'),
+            array('n', 'N', 'c', 'C'),
+            $string
+        );
+    
+        return $string;
     }
 }
